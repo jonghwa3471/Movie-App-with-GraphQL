7148a1f3090aa3620c8ee4843a10f67b6bd49bb7
Jonghwa <whdghk3471@gmail.com>
2026-01-07 15:13:18 +0900

    #1.1 ApolloProvider

--- src/routes/Movies.jsx
+++ src/routes/Movies.jsx
@@ -1,3 +1,29 @@
+import { gql } from "@apollo/client";
+import { useApolloClient } from "@apollo/client/react";
+import { useEffect, useState } from "react";
+
 export default function Movies() {
-  return <div>This is a list of movies.</div>;
+  const [movies, setMovies] = useState([]);
+  const client = useApolloClient();
+  useEffect(() => {
+    client
+      .query({
+        query: gql`
+          {
+            allMovies {
+              title
+              id
+            }
+          }
+        `,
+      })
+      .then((result) => setMovies(result.data.allMovies));
+  }, [client]);
+  return (
+    <div>
+      {movies.map((movie) => (
+        <li key={movie.id}>{movie.title}</li>
+      ))}
+    </div>
+  );
 }
